wayle-rs/wayle

fix: settings GUI layout wipe, symlinked config hot-reload, and CLI binary path resolution

wayle/src/cli/panel/settings.rs
@@ -17,7 +17,18 @@ use crate::cli::CliAction;
 pub async fn execute() -> CliAction {
     info!("Launching Wayle settings");
 
-    Command::new("wayle-settings")
+    let mut command = if let Ok(current_exe) = std::env::current_exe() {
+        let sibling = current_exe.parent().map(|p| p.join("wayle-settings"));
+        if let Some(ref sibling_path) = sibling && sibling_path.exists() {
+            Command::new(sibling_path)
+        } else {
+            Command::new("wayle-settings")
+        }
+    } else {
+        Command::new("wayle-settings")
+    };
+
+    command
         .stdin(Stdio::null())
         .stdout(Stdio::null())
         .stderr(Stdio::null())

DigitalPals/Lumen

(cherry picked from commit 81e22ae7dd8fcfdd07abac135ae4256884755017)

lumen/src/cli/panel/settings.rs
@@ -17,7 +17,18 @@ use crate::cli::CliAction;
 pub async fn execute() -> CliAction {
     info!("Launching Lumen settings");
 
-    Command::new("lumen-settings")
+    let mut command = if let Ok(current_exe) = std::env::current_exe() {
+        let sibling = current_exe.parent().map(|p| p.join("lumen-settings"));
+        if let Some(ref sibling_path) = sibling && sibling_path.exists() {
+            Command::new(sibling_path)
+        } else {
+            Command::new("lumen-settings")
+        }
+    } else {
+        Command::new("lumen-settings")
+    };
+
+    command
         .stdin(Stdio::null())
         .stdout(Stdio::null())
         .stderr(Stdio::null())